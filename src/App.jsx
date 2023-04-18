97419ce5ef019c307e51ee79eaaa3df0c5636295
Udhav Sharma <96074679+UdhavSharma2004@users.noreply.github.com>
2023-04-18 21:32:02 +0530

    deployment changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './Pages/Home';
 import Error404 from './Pages/Error404';
