b0a375837fe2e890b0b42f663387dba264548bfa
Udhav Sharma <96074679+UdhavSharma2004@users.noreply.github.com>
2023-04-28 14:17:36 +0530

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Vite-Redux-React_App/',
   plugins: [react()],
   server: {
     port: 3000,
